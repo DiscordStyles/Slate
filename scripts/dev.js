d13d383bb4e6d376f9bbe13f7c329cdfa3c722b5
Gibbu <Gibbu_@hotmail.com>
2021-07-24 14:36:15 +1000

    Fixed custom status, themed BD toasts.

--- scripts/dev.js
+++ scripts/dev.js
@@ -9,30 +9,30 @@ const autoprefixer = require('autoprefixer');
 const dataFolder = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share");
 const themesFolder = dataFolder + '\\BetterDiscord\\themes';
 
-const watcher = chokidar.watch('src', {persistent: true});
-watcher.on('change', () => {
-	sass.render({
-		file: 'src/Slate.theme.scss',
-		outputStyle: 'expanded'
-	}, (err, result) => {
-		if (err) {
-			console.error(err);
-			return false;
-		}
+chokidar.watch('src', {persistent: true})
+	.on('ready', () => console.log('Watching for changes...'))
+	.on('change', () => {
+		sass.render({
+			file: 'src/Slate.theme.scss',
+			outputStyle: 'expanded'
+		}, (err, result) => {
+			if (err) {
+				console.error(err);
+				return false;
+			}
 
-		const newRes = Buffer.from(result.css).toString();
+			const newRes = Buffer.from(result.css).toString();
 
-		postcss([autoprefixer])
-			.process(newRes, {
-				from: undefined,
-				to: undefined
-			})
-			.then(postcssRes => {
-				fs.writeFile(themesFolder+'\\Slate.theme.css', postcssRes.css, (err) => {
-					if (err) console.error(err);
-					else console.log(`Built css file. (${(result.stats.duration/60000 * 60).toFixed(2)}s)`);
+			postcss([autoprefixer])
+				.process(newRes, {
+					from: undefined,
+					to: undefined
 				})
-			})
-	})
-});
-console.log('Watching for changes...');
+				.then(postcssRes => {
+					fs.writeFile(themesFolder+'\\Slate.theme.css', postcssRes.css, (err) => {
+						if (err) console.error(err);
+						else console.log(`Built css file. (${(result.stats.duration/60000 * 60).toFixed(2)}s)`);
+					})
+				})
+		})
+	});
