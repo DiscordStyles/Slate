b4a52402b69d839dc1218ede9df35163c3a083ba
Celsiuss <celsiuss@await.sh>
2021-12-07 20:29:16 +0100

    consistent spacing on mentioned and reactioned messages. also improved dev script with --path flag

--- scripts/dev.js
+++ scripts/dev.js
@@ -6,7 +6,12 @@ const compile = require('./compile');
 const {name, dev} = require('./config.json');
 
 const dataFolder = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME + "/.local/share");
-const themesFolder = path.join(dataFolder, 'BetterDiscord', 'themes');
+let themesFolder = path.join(dataFolder, 'BetterDiscord', 'themes');
+
+const pathFlagIndex = process.argv.findIndex(el => el === '--path');
+if (pathFlagIndex !== -1) {
+	themesFolder = process.argv[pathFlagIndex + 1];
+}
 
 chokidar.watch('src', {persistent: true})
 	.on('ready', () => console.log(`Watching for changes...`))
