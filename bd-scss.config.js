7e5e649bb96e19ce338a8a97a606aea9ab269512
Ian <Gibbu_@hotmail.com>
2022-06-23 18:12:26 +1000

    Moved to new file structure.
    - Fixed #91.
    - Fixed #92.
    - Fixed #93.
    - Fixed #94.
    - Fixed #95.
    - Fixed #96.

---
+++ bd-scss.config.js
@@ -0,0 +1,15 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'Slate',
+		author: 'Gibbu & Tropical',
+		description: `An optimized, consistent, and functional theme for Discord based on GitHub's design language.`,
+		version: '1.1',
+		source: 'https://github.com/DiscordStyles/Slate/',
+		invite: 'TeRQEPb'
+	},
+	base: {
+		output: 'dist/dist'
+	},
+	baseImport: 'https://discordstyles.github.io/Slate/dist/Slate.css'
+}
